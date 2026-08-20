01af024edf398b799108939a64711f5f1c96c4d0
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2026-08-20 18:53:53 -0300

    feat:add setters to PedidoResponseDTO

--- src/main/java/com/danieloliveira/order_management/dto/response/PedidoResponseDTO.java
+++ src/main/java/com/danieloliveira/order_management/dto/response/PedidoResponseDTO.java
@@ -4,12 +4,14 @@ import com.danieloliveira.order_management.model.StatusPedido;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PedidoResponseDTO {
